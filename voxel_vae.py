e9ab522a32d08b9750b3f8f448621a0d1d83c212
Felix Friese <ffriese@techfak.uni-bielefeld.de>
2019-01-21 16:26:39 +0100

    plot reconstruction results during training and after, some small improvements to the network design

--- voxel_vae.py
+++ voxel_vae.py
@@ -2,12 +2,17 @@ import numpy as np
 import sys
 import os
 import tensorflow as tf
+import matplotlib.pyplot as plt
+#  This import registers the 3D projection, but is otherwise unused.
+# noinspection PyUnresolvedReferences
+from mpl_toolkits.mplot3d import Axes3D
 
-LATENT_DIMENSIONS = 7
+LATENT_DIMENSIONS = 10
+VOXEL_SPACE_SIZE = 16
 
 
 class VAE(object):
-    def __init__(self, input_shape, latent_dimensions, beta=1, batch_size=100, checkpoint_dir=None):
+    def __init__(self, input_shape, latent_dimensions, beta=2.5, batch_size=100, checkpoint_dir=None):
         self.current_best = None
         self.current_episode = None
         self.beta = beta
@@ -15,7 +20,7 @@ class VAE(object):
         self.batch_size = batch_size
         self.latent_dimensions = latent_dimensions
         # self.enc_in = tf.placeholder(tf.float32, (None,) + input_shape)
-        self.enc_in = tf.placeholder(tf.float32, (self.batch_size, ) + input_shape)
+        self.enc_in = tf.placeholder(tf.float32, (self.batch_size, ) + input_shape + (1, ))
         kernel = 4
         stride = (2, 2, 2)
         num_filter = 32
@@ -57,15 +62,18 @@ class VAE(object):
                                                         kernel_size=kernel)
         self.dec_out = tf.contrib.layers.conv3d_transpose(inputs=layer_de_4, num_outputs=1, stride=1,
                                                           kernel_size=1)
-
-        assert input_shape == self.dec_out.shape[1:]
+        assert self.enc_in.shape == self.dec_out.shape
         self.optimizer = tf.train.AdamOptimizer()
 
         kl_loss = 1 + self.log_var - tf.pow(self.mus, 2) - tf.exp(self.log_var)
         kl_loss = -0.5 * tf.reduce_sum(kl_loss, axis=-1)
         reconstruction_err = tf.losses.mean_squared_error(labels=self.enc_in, predictions=self.dec_out)
-        reconstruction_err * np.prod(self.input_shape[:-1])
+        reconstruction_err *= np.prod(self.input_shape)
         self.loss = tf.reduce_mean(self.beta * kl_loss + reconstruction_err)
+
+        # reconstruction_err = tf.nn.sigmoid_cross_entropy_with_logits(labels=self.enc_in, logits=self.dec_out)
+        # self.loss = tf.reduce_mean(reconstruction_err)
+
         self.train_op = self.optimizer.minimize(self.loss)
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
@@ -82,13 +90,35 @@ class VAE(object):
         self._cur_err = None
         self._cur_loss = None
 
-    def train(self, num_episodes, data):
+    def train(self, num_episodes, data, plot_best=False):
         self._train_data_len = len(data)
         if self.current_best is None:
+            self._check_dir(self.checkpoint_dir, create_on_fail=True)
+            with open('{}/history.csv'.format(self.checkpoint_dir), 'w') as file:
+                file.write('train/success_rate,test/success_rate,best\n')
             self.current_best = sys.float_info.max
         if self.current_episode is None:
             self.current_episode = 0
         print('starting with episode %s, training additional %s episodes' % (self.current_episode, num_episodes))
+        if plot_best:
+            plt.ion()
+            fig = plt.figure('Reconstruction')
+            ax = fig.gca(projection='3d')
+            ax.view_init(24, 144)
+            recon = self.full_pass(data[:self.batch_size])[0].reshape((self.batch_size, ) + self.input_shape)
+            print('initial: max: %s min: %s mean: %s' % (np.max(recon), np.min(recon), np.mean(recon)))
+            ax.voxels(data[0] > 0.5, facecolors=[0, 0, 1, 0.3], edgecolor=[0, 0, 1, 0.0])
+            # ax.voxels(recon[0] > 0.5, facecolors=[0, 1, 0, 0.2], edgecolor=[0, 1, 0, 0.5])
+            # overlap green
+            ax.voxels(np.logical_and(data[0] > 0.5, recon[0] > 0.5), facecolors=[0, 1, 0, 0.0],
+                      edgecolor=[0, 1, 0, 0.7])
+            # orig only blue
+            # ax.voxels(np.logical_and(data[0] > 0.5, recon[0] <= 0.5), facecolors=[0, 0, 1, 0.4],
+            # edgecolor=[0, 0, 1, 0.0])
+            # recon only red
+            ax.voxels(np.logical_and(data[0] <= 0.5, recon[0] > 0.5), facecolors=[1, 0, 0, 0.0],
+                      edgecolor=[1, 0, 0, 0.7])
+            plt.pause(0.0000001)
         with self.sess as session:
             for i in range(num_episodes):
                 print('episode %s of %s' % (self.current_episode, (num_episodes-i+self.current_episode)))
@@ -96,10 +126,11 @@ class VAE(object):
                 for j in range(0, self._train_data_len, self.batch_size):
                     if self._train_data_len > j + self.batch_size:
                         batch = np.expand_dims(data[j:j + self.batch_size], axis=-1)
-                        _, loss, dec_out = session.run([self.train_op, self.loss, self.dec_out], feed_dict={self.enc_in: batch})
+                        _, loss, dec_out, log_var = session.run([self.train_op, self.loss, self.dec_out, self.log_var],
+                                                                feed_dict={self.enc_in: batch})
 
-                        batch = batch.reshape(self.input_shape + (self.batch_size, ))
-                        dec_out = dec_out.reshape(self.input_shape + (self.batch_size, ))
+                        batch = batch.reshape((self.batch_size, ) + self.input_shape)
+                        dec_out = dec_out.reshape((self.batch_size, ) + self.input_shape)
                         self._cur_err = '%0.8f' % float(np.sum(np.abs(batch-dec_out))/self.batch_size)
                         self._cur_loss = '%0.8f' % loss
 
@@ -110,11 +141,40 @@ class VAE(object):
                         print(
                             'trained {_trained_ep_samples} of {_train_data_len} samples '
                             'for episode {_cur_ep_str} (loss:{_cur_loss}, err:{_cur_err}'.format(**self.__dict__))
-                        self._save(loss=self._cur_loss, episode=self.current_episode)
+                        print('sigmas:', np.mean(np.exp(0.5 * log_var), axis=0))
                         if loss < self.current_best:
                             self.current_best = loss
                             print('>>>>>>>> new best: %r' % self.current_best)
                             self._save('best', loss=loss, episode=self.current_episode)
+                            if plot_best:
+                                errs = [np.sum(s) for s in np.sum(np.abs(batch-dec_out), axis=1)]
+                                num_vox_orig = [np.sum(s) for s in np.sum(batch, axis=1)]
+                                num_vox_recon = [np.sum(s) for s in np.sum(dec_out > 0.5, axis=1)]
+                                print(np.max(dec_out))
+                                best_idx = int(np.argmin(errs))
+                                ax.cla()
+                                plt.title('err: %0.3f, orig_vox: %s, rec_vox: %s' %
+                                          (errs[best_idx], num_vox_orig[best_idx], num_vox_recon[best_idx]))
+                                ax.voxels(batch[best_idx] > 0.5, facecolors=[0, 0, 1, 0.5],
+                                          edgecolor=[1, 0, 0, 0.0])
+                                 #ax.voxels(dec_out[best_idx] > 0.5, facecolors=[0, 1, 0, 0.2],
+                                # edgecolor=[0, 1, 0, 0.5])
+                                # overlap green
+                                ax.voxels(np.logical_and(batch[best_idx] > 0.5, dec_out[best_idx] > 0.5),
+                                          facecolors=[0, 1, 0, 0.0],
+                                          edgecolor=[0, 1, 0, 0.9])
+                                # orig only blue
+                                #ax.voxels(np.logical_and(batch[best_idx] > 0.5, dec_out[best_idx] <= 0.5),
+                                # facecolors=[0, 0, 1, 0.4],
+                                #          edgecolor=[0, 0, 1, 0.0])
+                                # recon only red
+                                ax.voxels(np.logical_and(batch[best_idx] <= 0.5, dec_out[best_idx] > 0.5),
+                                          facecolors=[1, 0, 0, 0.0],
+                                          edgecolor=[1, 0, 0, 0.9])
+                                plt.pause(0.0000001)
+
+                        self._save(loss=self._cur_loss, episode=self.current_episode)
+
                 '{0:0{width}}'.format(5, width=3)
                 self.current_episode += 1
 
@@ -143,6 +203,9 @@ class VAE(object):
             file.write('%s' % loss)
         with open('{}episode'.format(path), 'w') as file:
             file.write('%s' % episode)
+        if save_dir == 'latest':
+            with open('{}/history.csv'.format(self.checkpoint_dir), 'a') as file:
+                file.write('{},{},{}\n'.format(episode, loss, self.current_best))
 
     def _load(self, load_dir):
         path = '{}/{}/'.format(self.checkpoint_dir, load_dir)
@@ -172,30 +235,25 @@ class VAE(object):
 def train_that_data(voxel_filename):
     _batch_size = 48
 
-    vae = VAE((16, 16, 16, 1), LATENT_DIMENSIONS, batch_size=_batch_size)
+    vae = VAE((VOXEL_SPACE_SIZE, VOXEL_SPACE_SIZE, VOXEL_SPACE_SIZE), LATENT_DIMENSIONS, batch_size=_batch_size)
     train_data = np.load(voxel_filename)
     vae.load()
 
-    vae.train(num_episodes=100, data=train_data)
+    vae.train(num_episodes=100, data=train_data, plot_best=True)
 
 
 def plot_that_data(voxel_filename):
-    import matplotlib
-    matplotlib.use('TkAgg')
-    import matplotlib.pyplot as plt
-    #  This import registers the 3D projection, but is otherwise unused.
-    # noinspection PyUnresolvedReferences
-    from mpl_toolkits.mplot3d import Axes3D
 
     _batch_size = 100
     display_random_decoded = True
 
-    vae = VAE((16, 16, 16, 1), latent_dimensions=LATENT_DIMENSIONS, batch_size=_batch_size)
+    vae = VAE((VOXEL_SPACE_SIZE, VOXEL_SPACE_SIZE, VOXEL_SPACE_SIZE), latent_dimensions=LATENT_DIMENSIONS,
+              batch_size=_batch_size)
     data = np.load(voxel_filename)
     vae.load()
 
     data_set = data[:_batch_size]
-    results = vae.full_pass(data_set)[0].reshape((_batch_size, 16, 16, 16))
+    results = vae.full_pass(data_set)[0].reshape((_batch_size, VOXEL_SPACE_SIZE, VOXEL_SPACE_SIZE, VOXEL_SPACE_SIZE))
 
     plt.ion()
     fig = plt.figure('Reconstruction')
@@ -203,8 +261,11 @@ def plot_that_data(voxel_filename):
     ax.view_init(24, 144)
 
     if display_random_decoded:
-        random_decoded = vae.decode(np.random.rand(_batch_size, vae.latent_dimensions))[0].reshape((_batch_size,
-                                                                                                 16, 16, 16))
+        random_decoded = vae.decode(np.random.rand(_batch_size,
+                                                   vae.latent_dimensions))[0].reshape((_batch_size,
+                                                                                       VOXEL_SPACE_SIZE,
+                                                                                       VOXEL_SPACE_SIZE,
+                                                                                       VOXEL_SPACE_SIZE))
         fig2 = plt.figure('Generative')
         ax2 = fig2.gca(projection='3d')
         ax2.view_init(24, 144)
@@ -214,18 +275,18 @@ def plot_that_data(voxel_filename):
         orig = data_set[i]
         recon = results[i]
         ax.cla()
-        ax.voxels(orig > 0, facecolors=[1, 0, 0, 0.85], edgecolor=[1, 0, 0, 0.0])
-        ax.voxels(recon > 0, facecolors=[0, 1, 0, 0.2], edgecolor=[0, 1, 0, 0.0])
+        ax.voxels(orig > 0.5, facecolors=[1, 0, 0, 0.5], edgecolor=[1, 0, 0, 0.9])
+        ax.voxels(recon > 0.5, facecolors=[0, 1, 0, 0.2], edgecolor=[0, 1, 0, 0.5])
         if display_random_decoded:
             ax2.cla()
-            ax2.voxels(random_decoded[i] > 0, facecolors=[0, 0, 1, 0.85], edgecolor=[0, 1, 0, 0.0])
+            ax2.voxels(random_decoded[i] > 0.5, facecolors=[0, 0, 1, 0.5], edgecolor=[0, 0, 1, 0.9])
 
-        print('reconstruction err:', np.sum(np.abs(orig - recon)))
+        print('reconstruction err:', np.sum((orig > 0.5) ^ (recon > 0.5)))
         plt.pause(0.0000000001)
 
     plt.pause(1)
 
 
 if __name__ == '__main__':
-    plot_that_data("/home/ffriese/prj-robotic-arms/voxel_vae/transformed voxel_data.npy")
-    #train_that_data("/home/ffriese/prj-robotic-arms/voxel_vae/transformed voxel_data.npy")
+    # plot_that_data("/home/ffriese/prj-robotic-arms/voxel_vae/transformed voxel_data.npy")
+    train_that_data("/home/ffriese/prj-robotic-arms/voxel_vae/transformed voxel_data.npy")
